c8f858241875a29996dcdf39bf07e24cd5fc6fa0
Mmaryyy <marynrvly@gmail.com>
2023-05-12 14:57:37 +0900

    [style] grid 바둑판 갤러리 구현 수정

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -4,13 +4,17 @@ import styled from 'styled-components'
 const Wrapper = styled.div`
   overflow: hidden;
   width: 100%;
-  height: 113px;
+  position: relative;
+  ::after {
+    display: block;
+    content: '';
+    padding-bottom: 100%;
+  }
 `
 
 const Img = styled.img`
-  display: block;
+  position: absolute;
   width: 100%;
-  max-width: 100%;
   height: 100%;
   object-fit: cover;
 `
